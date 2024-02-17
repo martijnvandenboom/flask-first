c89518ee585cf8eb0ff69a4ba1fe4a1e5ad4436c
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2024-02-17 13:46:15 +0100

    port changed to default 5000
    port changed to default 5000

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello, World! My name is: Martijn van den Boom'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
